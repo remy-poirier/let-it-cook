64f0c2a841a35ccb40d0fb70446e3c698841a048
Rémy Poirier <remy.poirier@particeep.com>
2024-10-30 14:30:57 +0100

    Minor update on hook

--- src/hooks/useNewsFeed.ts
+++ src/hooks/useNewsFeed.ts
@@ -11,56 +11,63 @@ interface NewsFeedEntry {
   amount: number
 }
 
-export const useNewsFeed = (nbToKeep?: number) => {
+export const useNewsFeed = (
+  nbToKeep?: number,
+  kind?: 'INVESTMENT' | 'DIVIDEND',
+) => {
   const data = dataJson as Data
 
   const newsFeed: NewsFeedEntry[] = []
 
-  data.ldds_credit_agricole
-    .transactions.forEach((transaction) => {
-      newsFeed.push({
-        kind: 'INVESTMENT',
-        label: 'LDD Solidaire Crédit Agricole',
-        description: transaction.label,
-        amount: transaction.amount,
-        date: transaction.date,
+  if (!kind || kind === 'INVESTMENT') {
+    data.ldds_credit_agricole
+      .transactions.forEach((transaction) => {
+        newsFeed.push({
+          kind: 'INVESTMENT',
+          label: 'LDD Solidaire Crédit Agricole',
+          description: transaction.label,
+          amount: transaction.amount,
+          date: transaction.date,
+        })
       })
-    })
 
-  data.bricks
-    .transactions.forEach((transaction) => {
-      newsFeed.push({
-        kind: 'INVESTMENT',
-        label: 'Bricks',
-        description: transaction.label,
-        amount: transaction.amount,
-        date: transaction.date,
+    data.bricks
+      .transactions.forEach((transaction) => {
+        newsFeed.push({
+          kind: 'INVESTMENT',
+          label: 'Bricks',
+          description: transaction.label,
+          amount: transaction.amount,
+          date: transaction.date,
+        })
       })
-    })
 
-  data.bricks
-    .dividends.forEach((dividend) => {
-      const totalDividend = TransactionsUtils.realEstate.totalNetProfitabilityForDividendEntry(dividend)
-      const tax = TransactionsUtils.realEstate.taxForDividendEntry(dividend)
-      newsFeed.push({
-        kind: 'DIVIDEND',
-        label: 'Bricks',
-        description: `Fiscalité déduite: ${currencyFormatter.format(Math.abs(tax))}`,
-        amount: totalDividend,
-        date: dividend.date,
+    data.epsor
+      .transactions.forEach((transaction) => {
+        newsFeed.push({
+          kind: 'INVESTMENT',
+          label: 'Epsor',
+          description: transaction.label,
+          amount: transaction.amount,
+          date: transaction.date,
+        })
       })
-    })
+  }
 
-  data.epsor
-    .transactions.forEach((transaction) => {
-      newsFeed.push({
-        kind: 'INVESTMENT',
-        label: 'Epsor',
-        description: transaction.label,
-        amount: transaction.amount,
-        date: transaction.date,
+  if (!kind || kind === 'DIVIDEND') {
+    data.bricks
+      .dividends.forEach((dividend) => {
+        const totalDividend = TransactionsUtils.realEstate.totalNetProfitabilityForDividendEntry(dividend)
+        const tax = TransactionsUtils.realEstate.taxForDividendEntry(dividend)
+        newsFeed.push({
+          kind: 'DIVIDEND',
+          label: 'Bricks',
+          description: `Fiscalité déduite: ${currencyFormatter.format(Math.abs(tax))}`,
+          amount: totalDividend,
+          date: dividend.date,
+        })
       })
-    })
+  }
 
   // Sort them by date, form the most recent to the oldest
   newsFeed.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
